525d8ff9447e9aeb633fd0c27fed568572eb1d5f
michaelschoerghuber09-cell <michaelschoerghuber09@gmail.com>
2026-07-22 23:50:10 +0200

    Finished the version selection
    I finished the version selection for the rest of the server option. It was just copy and paste

--- src/pages/SetUpServer/NewServer.tsx
+++ src/pages/SetUpServer/NewServer.tsx
@@ -11,13 +11,17 @@ export default function NewServer() {
             {/*Select the type of server you want to create toogle switch to show you all avaible options from the api */}
             <div className="selectServerOptionContainer">
                 <button className="selectServerVariantBtn" onClick={()=> setActiveCategory("vanilla")}>Vanilla</button>
-                <button className="selectServerVariantBtn">Modded</button>
-                <button className="selectServerVariantBtn">Plugin Server</button>
-                <button className="selectServerVariantBtn">Bedrock</button>
-                <button className="selectServerVariantBtn">Proxies</button> {/*I have to look into proxies server and how they are setup */}
+                <button className="selectServerVariantBtn" onClick={()=> setActiveCategory("modded")}>Modded</button>
+                <button className="selectServerVariantBtn" onClick={()=> setActiveCategory("servers")}>Plugin</button>
+                <button className="selectServerVariantBtn" onClick={()=> setActiveCategory("bedrock")}>Bedrock</button>
+                <button className="selectServerVariantBtn" onClick={()=>setActiveCategory("proxies")}>Proxy</button> {/*I have to look into proxies server and how they are setup */}
             </div>
         </nav>
             {activeCategory === "vanilla" && <VanillaServerOption />}
+            {activeCategory === "modded" && <ModdedServerOption />}
+            {activeCategory === "servers" && <PluginServerOption />}
+            {activeCategory === "bedrock" && <BedrockServerOption />}
+            {activeCategory === "proxies" && <ProxyServerOption />}
         </>
     );
 }
@@ -25,18 +29,20 @@ export default function NewServer() {
 function VersionSelector({
     type,
     variant,
+    os,
     selectId,
     selectName,
 }: {
     type: string;
     variant: string;
+    os?:string;
     selectId: string;
     selectName: string;
 }) {
     const [versions, setVersions] = useState<string[]>([]);
 
     useEffect(() => {
-        getAllVersions(type, variant)
+        getAllVersions(type, variant, os!)
             .then(setVersions)
             .catch((err) => console.error("Fetch failed:", err));
     }, [type, variant]);
@@ -57,7 +63,7 @@ export function VanillaServerOption() {
     return(
         <div className="vanillaServerOptions">
             <div className="vanillaServerRelease">
-                <h1>Vanilla Server</h1>
+                <h1>Vanilla</h1>
 
                     <label htmlFor="version">Choose your version:</label>
                         <VersionSelector
@@ -69,13 +75,184 @@ export function VanillaServerOption() {
             </div>
 
             <div className="snapshotServer">
-                <h1>Snapshot Server</h1>
+                <h1>Snapshot</h1>
                     <label htmlFor="version">Choose your version:</label>
                         <VersionSelector
                             type="vanilla"
                             variant="snapshot"
                             selectId="versionSnapshot"
                             selectName="version-selector-snapshot"
+                        />
+            </div>
+        </div>
+    );
+}
+
+export function ModdedServerOption() {
+
+    return(
+        <div className="moddedServerOptions">
+            <div className="moddedServerForge">
+                <h1>Forge</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="modded"
+                            variant="forge"
+                            selectId="versionForge"
+                            selectName="version-selector-forge"
+                        />
+            </div>
+
+            <div className="moddedServerNeoForged">
+                <h1>NeoForged</h1>
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="modded"
+                            variant="neoforge"
+                            selectId="versionNeoForge"
+                            selectName="version-selector-neoforge"
+                        />
+            </div>
+
+            <div className="moddedServerFabric">
+                <h1>Fabric</h1>
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="modded"
+                            variant="fabric"
+                            selectId="versionFabric"
+                            selectName="version-selector-fabric"
+                        />
+            </div>
+        </div>
+    );
+}
+
+export function PluginServerOption() {
+
+    return(
+        <div className="pluginServerOptions">
+            <div className="pluginServerPaper">
+                <h1>Paper</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="servers"
+                            variant="paper"
+                            selectId="versionPaper"
+                            selectName="version-selector-paper"
+                        />
+            </div>
+
+            <div className="pluginServerPurper">
+                <h1>Purper</h1>
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="servers"
+                            variant="purpur"
+                            selectId="versionPurpur"
+                            selectName="version-selector-purpur"
+                        />
+            </div>
+
+            <div className="pluginServerLeaf">
+                <h1>Leaf</h1>
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="servers"
+                            variant="leafmc"
+                            selectId="versionLeaf"
+                            selectName="version-selector-leaf"
+                        />
+            </div>
+
+            <div className="pluginServerFolia">
+                <h1>Folia</h1>
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="servers"
+                            variant="folia"
+                            selectId="versionFolia"
+                            selectName="version-selector-folia"
+                        />
+            </div>
+        </div>
+    );
+}
+
+export function BedrockServerOption() {
+
+    return(
+        <div className="bedrockServerOptions">
+            <div className="bedrockServerLatestLinux">
+                <h1>Bedrock Latest Linux</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="bedrock"
+                            variant="latest"
+                            os="linux"
+                            selectId="versionBedrockLatestLinux"
+                            selectName="version-selector-bedrock-latest-linux"
+                        />
+            </div>
+
+            <div className="bedrockServerLatestWindows">
+                <h1>Bedrock Latest Windows</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="bedrock"
+                            variant="latest"
+                            os="windows"
+                            selectId="versionBedrockLatestWindoes"
+                            selectName="version-selector-bedrock-latest-windows"
+                        />
+            </div>
+
+            <div className="bedrockServerPreviewLinux">
+                <h1>Bedrock Preview Linux</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="bedrock"
+                            variant="preview"
+                            os="linux"
+                            selectId="versionBedrockPreviewLinux"
+                            selectName="version-selector-bedrock-preview-linux"
+                        />
+            </div>
+
+            <div className="bedrockServerPreviewWindows">
+                <h1>Bedrock Preview Windows</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="bedrock"
+                            variant="preview"
+                            os="windows"
+                            selectId="versionBedrockPreviewWindows"
+                            selectName="version-selector-bedrock-preview-windows"
+                        />
+            </div>
+        </div>
+    );
+}
+
+export function ProxyServerOption() {
+
+    return(
+        <div className="proxyServerOptions">
+            <div className="proxyServer">
+                <h1>Velocity</h1>
+
+                    <label htmlFor="version">Choose your version:</label>
+                        <VersionSelector
+                            type="proxies"
+                            variant="velocity"
+                            selectId="versionVelocity"
+                            selectName="version-selector-velocity"
                         />
             </div>
         </div>
--- src/pages/SetUpServer/getAllVersions.ts
+++ src/pages/SetUpServer/getAllVersions.ts
@@ -1,7 +1,9 @@
 import { fetch } from "@tauri-apps/plugin-http";
 
-export default async function getAllVersions(type:string, variant:string): Promise<string[]> {
-    const data = await fetch(`https://mcjarfiles.com/api/get-versions/${type}/${variant}`);
+export default async function getAllVersions(type:string, variant:string, os: string): Promise<string[]> {
+
+    const data = (os === "linux" || os === "windows") ? await fetch(`https://mcjarfiles.com/api/get-versions/${type}/${variant}/${os}`) : await fetch(`https://mcjarfiles.com/api/get-versions/${type}/${variant}`);
+    
     if(!data.ok) {
         throw new Error (
             `Error while loading the versions (${data.status} ${data.statusText})`
